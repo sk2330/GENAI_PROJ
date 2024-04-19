4aec174dabf1ed4553efc9458a356d7cc5ce8ed6
unknown <saikiven03@gmail.com>
2024-04-19 14:37:19 +0530

    App.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import docx2txt
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_pdf_text(pdf_docs):
     text = ""
